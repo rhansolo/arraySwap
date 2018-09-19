339d817a6be2cf27ebcecd2319731948fcf85736
robinhanstuy <rhan@stuy.edu>
2018-09-19 16:30:15 -0400

    hw finished

--- work02.c
+++ work02.c
@@ -8,7 +8,21 @@ int main(){
     arr[i] = rand();
   }
   arr[9] = 0;
+  printf("Orignal Array:\n");
   for (int i = 0; i < 10; i++){
-    printf("%d \n",arr[i]);
+    printf("%d\n",arr[i]);
   }
+  int arr2[10];
+  int * a = arr;
+  int * b = arr2 + 10;
+  for (int i = 0; i < 10; i ++){
+    b--;
+    *b = *a;
+    a++;
+  }
+  printf("Reversed array: \n");
+  for (int i = 0; i < 10; i++){
+    printf("%d\n",arr2[i]);
+  }
+  return 0;
 }
